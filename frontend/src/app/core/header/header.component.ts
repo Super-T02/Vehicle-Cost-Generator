3988534182f89cf05ad16f7f2ba5211fd9a2b7c2
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2021-12-08 19:53:17 +0100

    WE-49: fixed header for selecting the route

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -1,19 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ResizeService} from '../services/resize.service';
 import {MEDIA_BREAKPOINTS} from '../../../environments/constants';
 import {AuthService} from '../services/auth.service';
+import {LastRouteService} from '../services/last-route.service';
+import {Event, RouterEvent, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.less']
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
 
   breakPoints = MEDIA_BREAKPOINTS;
+  url: string = '';
 
-  constructor(public resizeService: ResizeService,
-              public auth: AuthService) {
+  constructor(
+    public resizeService: ResizeService,
+    public auth: AuthService,
+    public lastRoute: LastRouteService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter((e: Event): e is RouterEvent => e instanceof RouterEvent)
+    ).subscribe((e: RouterEvent) => {
+      this.url = e.url;
+    });
   }
 
 }
