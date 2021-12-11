cf745bc7bef78d2820e1643e24f2d9f5850ddf84
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2021-12-11 18:41:47 +0100

    WE-51: changed some things at the home page

--- frontend/src/app/pages/welcome/welcome.module.ts
+++ frontend/src/app/pages/welcome/welcome.module.ts
@@ -7,20 +7,22 @@ import {NzBreadCrumbModule} from 'ng-zorro-antd/breadcrumb';
 import {NzCardModule} from 'ng-zorro-antd/card';
 import {NzDividerModule} from 'ng-zorro-antd/divider';
 import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 
 @NgModule({
 	declarations: [
 		WelcomePageComponent
 	],
-	imports: [
-		SharedModule,
-		WelcomeRoutingModule,
-		NzBreadCrumbModule,
-		NzCardModule,
-		NzDividerModule,
-		NzIconModule
-	]
+    imports: [
+        SharedModule,
+        WelcomeRoutingModule,
+        NzBreadCrumbModule,
+        NzCardModule,
+        NzDividerModule,
+        NzIconModule,
+        NzButtonModule
+    ]
 })
 export class WelcomeModule {
 }
